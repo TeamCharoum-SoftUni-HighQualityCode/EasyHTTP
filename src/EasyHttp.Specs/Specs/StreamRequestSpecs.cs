e01c51d79c318fef059b3870de25adde9c57ba21
Svetlozar Kirkov <svetlozark@gmail.com>
2015-07-31 11:59:48 +0300

    initial upload of the project

---
+++ src/EasyHttp.Specs/Specs/StreamRequestSpecs.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Reflection;
+using EasyHttp.Http;
+using Machine.Specifications;
+
+namespace EasyHttp.Specs.Specs
+{
+    [Subject(typeof(HttpClient))]
+    public class when_making_a_GET_with_stream_response_true
+    {
+        // TODO: Implement me
+        static HttpClient httpClient;
+    }
+}
