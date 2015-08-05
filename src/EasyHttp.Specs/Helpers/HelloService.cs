9197214b9622a431da479dd274d08d1aff166c81
denitsadconsult <denitsa.dobreva@d-consult.net>
2015-08-05 13:50:16 +0300

    started extracting classes to EasyHttp.Specs.Helpers

---
+++ src/EasyHttp.Specs/Helpers/HelloService.cs
@@ -0,0 +1,39 @@
+﻿using ServiceStack.ServiceInterface;
+
+namespace EasyHttp.Specs.Helpers
+{
+    internal class HelloService: RestServiceBase<Hello>
+    {
+        public override object OnGet(Hello request)
+        {
+            return new HelloResponse
+            {
+                Result = "Hello, " + request.Name
+            };
+        }
+
+        public override object OnPut(Hello request)
+        {
+            return new HelloResponse
+            {
+                Result = "Hello, " + request.Name
+            };
+        }
+
+        public override object OnPost(Hello request)
+        {
+            return new HelloResponse
+            {
+                Result = "Hello, " + request.Name
+            };
+        }
+
+        public override object OnDelete(Hello request)
+        {
+            return new HelloResponse
+            {
+                Result = "Hello, " + request.Name
+            };
+        }
+    }
+}
---
+++ src/EasyHttp.Specs/Helpers/RedirectorService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceInterface;
+
+namespace EasyHttp.Specs.Helpers
+{
+    internal class RedirectorService : RestServiceBase<Redirect>
+    {
+        public override object OnGet(Redirect request)
+        {
+            if (this.Request.AbsoluteUri.EndsWith("redirected"))
+            {
+                return new HttpResult()
+                {
+                    StatusCode = HttpStatusCode.OK,
+                };
+            }
+
+            return new HttpResult()
+            {
+                StatusCode = HttpStatusCode.Redirect,
+                Location = this.Request.AbsoluteUri + "/redirected"
+            };
+        }
+    }
+}
